9f2d7f0b6d69f88bed0dfeb27b03bc33559679e7
‘Tangjijun’ <3414939316@qq.com>
2023-03-12 17:51:51 +0800

    1

--- src/main/java/prog2/exercise4/flight/booking/system/FlightBooking.java
+++ src/main/java/prog2/exercise4/flight/booking/system/FlightBooking.java
@@ -1,3 +1,234 @@
 package prog2.exercise4.flight.booking.system;
+import java.time.LocalDate;
+public class FlightBooking {
+    private String TripSource;
+    private String BookingClass;
+    private String passengerFullName;
+    private String sourceAirport;
+    private String FlightCompany;
+    private String tripDestination;
+    private String destinationAirport;
+    private String TripType;
+    private LocalDate departureDate;
+    private LocalDate returnDate;
+    private int childPassengers;
+    private int adultPassengers;
+    private int totalPassengers;
+    private double departingTicketPrice;
+    private double totalTicketPrice;
+    public FlightBooking(String aNull, LocalDate depart, LocalDate returnDate, int i, int i1) {
+        this.passengerFullName = aNull;
+        this.departureDate = depart;
+        this.returnDate = returnDate;
+        this.childPassengers =i;
+        this.adultPassengers =i1;
+    }
 
+    public void setTripType(int b) {
+        if (b == 1)
+        {
+            TripType = "Oneway";
+        }
+        if (b == 2){
+            TripType="return";
+        }
+    }
+
+    public String getTripType() {
+        return TripType;
+    }
+
+    public String getBookingClass() {
+        return BookingClass;
+    }
+
+    public void setBookingClass(int a) {
+        if (a == 1){
+            BookingClass = "First";
+        }
+        if (a == 2){
+            BookingClass ="Bussiness";
+        }
+        if (a == 3){
+            BookingClass= "Economy";
+        }
+    }
+
+    public Object getTripSource() {
+        return TripSource;
+    }
+
+    public void setTripSource(String tripSource) {
+        this.TripSource = tripSource;
+    }
+
+    public String getFlightCompany() {
+        return FlightCompany;
+    }
+
+    public String getPassengerFullName() {
+        return passengerFullName;
+    }
+
+    public void setPassengerFullName(String passengerFullName) {
+
+        this.passengerFullName = passengerFullName;
+    }
+
+
+    public void setFlightCompany(String flightCompany) {
+        FlightCompany = flightCompany;
+    }
+
+    public String getSourceAirport() {
+        return sourceAirport;
+    }
+
+    public void setSourceAirport(String sourceAirport) {
+        this.sourceAirport = sourceAirport;
+    }
+
+    public String getTripDestination() {
+        return tripDestination;
+    }
+
+    public void setTripDestination(String tripDestination) {
+        this.tripDestination = tripDestination;
+    }
+
+    public String getDestinationAirport() {
+        return destinationAirport;
+    }
+
+    public void setDestinationAirport(String destinationAirport) {
+        this.destinationAirport = destinationAirport;
+    }
+
+    public LocalDate getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(LocalDate departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public int getChildPassengers() {
+        return childPassengers;
+    }
+
+    public void setChildPassengers(int childPassengers) {
+        this.childPassengers = childPassengers;
+    }
+
+    public int getAdultPassengers() {
+        return adultPassengers;
+    }
+
+    public void setAdultPassengers(int adultPassengers) {
+        this.adultPassengers = adultPassengers;
+    }
+
+    public int getTotalPassengers() {
+        return totalPassengers;
+    }
+
+    public void setTotalPassengers(int totalPassengers) {
+        this.totalPassengers = totalPassengers;
+    }
+
+    public double getDepartingTicketPrice() {
+        return departingTicketPrice;
+    }
+
+    public void setDepartingTicketPrice(double departingTicketPrice) {
+        this.departingTicketPrice = departingTicketPrice;
+    }
+
+    public double getTotalTicketPrice() {
+        return totalTicketPrice;
+    }
+
+    public void setTotalTicketPrice(double totalTicketPrice) {
+        this.totalTicketPrice = totalTicketPrice;
+    }
+
+    private String ticketNumber;
+    public String getTicketNumber() {
+        return ticketNumber;
+    }
+
+    public void setTicketNumber(String ticketNumber) {
+        this.ticketNumber = ticketNumber;
+    }
+    public String toString(){
+        return "Dear " + passengerFullName + ". Thank you for booking your flight with " +
+                FlightCompany + ". \nFollowing are the details of your booking and the trip:" + "\n" +
+                "Ticket Number: " + ticketNumber + "\n" +
+                "From " + TripSource + " to " + tripDestination + "\n" +
+                "Date of departure: " +departureDate + "\n" +
+                "Date of return: " + returnDate + "\n" +
+                "Total passengers: " + totalPassengers + "\n" +
+                "Total ticket price in Euros: " + totalTicketPrice;
+    }
+
+    public void setTotalPassengers(int i, int i1) {
+    }
+
+
+    public void setReturnTicketPrice() {
+    }
+
+    public void setTripDestination(String s, String s1) {
+    }
+
+    public void setTripType(String s) {
+    }
+
+    public void setBookingClass(String s) {
+    }
+
+    public void setDepartingTicketPrice(int child, int adults) {
+    }
+
+    public void setTotalTicketPrice() {
+    }
+
+    public enum TripSource {
+        ;
+
+        public static final FlightBooking.TripSource NANJING = null;
+        public static final FlightBooking.TripSource BEIJING = null;
+        public static final FlightBooking.TripSource SHANGHAI = null;
+        public static final FlightBooking.TripSource OULU= null;
+        public static final FlightBooking.TripSource HELSINKI = null;
+        public static final FlightBooking.TripSource PARIS = null;
+        
+        
+    }
+
+    public enum BookingClass {
+        ;
+
+        public static final FlightBooking.BookingClass FIRST = null;
+    }
+
+    public interface TripDestination {
+
+        TripDestination BEIJING = null;
+    }
+
+    public interface TripType {
+        FlightBooking.TripType ONE_WAY = null;
+
+        FlightBooking.TripType RETURN = null;
+    }
+}
 
--- src/main/java/prog2/exercise4/flight/booking/system/Main.java
+++ src/main/java/prog2/exercise4/flight/booking/system/Main.java
@@ -1,5 +1,8 @@
 package prog2.exercise4.flight.booking.system;
-
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Random;
+import java.util.Scanner;
 /**
  * Hello world!
  *
@@ -8,7 +11,41 @@ public class Main
 {
     public static void main( String[] args )
     {
+        String TripSource = "NANJING";
+        String sdepart = "2023-03-09";
+        LocalDate depart = LocalDate.parse(sdepart);
+        String sreturn = "2023-03-14";
+        LocalDate returnDate = LocalDate.parse(sreturn);
+        String random = "FOF"+Math.random();// flightID
+        String substring = random.substring(random.length()-4);
+        String sourceAirport = "NANJING LUKOU \n" +
+                "INTERNATIONAL AIRPORT";
+        String destinationAirport = "OULU AIRPORT";
+        FlightBooking a = new FlightBooking("Kehan", depart, returnDate, 1, 3);
+        int expResult = 4;
+        a.setTotalPassengers(1,3);
+        int result = a.getTotalPassengers();
+        a.setPassengerFullName("Kehan");
+        a.setTicketNumber("HT2514875");
+        a.setTripSource("NanJing") ;
+        a.setTripDestination("OULU");
+        a.setFlightCompany("Flights-of-Fancy");
+        a.setDepartureDate(LocalDate.parse("2023-03-09"));
+        a.setReturnDate(LocalDate.parse("2023-03-14"));
+        a.setTotalTicketPrice(6666);
+        a.setTripSource("NANJING");
+
+
+        System.out.println("Dear " + a.getPassengerFullName() + ". Thank you for booking your flight with " +
+                a.getFlightCompany() + ".\n Following are the details of your booking and the trip:" + "\n" +
+                "Ticket Number: " + a.getTicketNumber() + "\n" +
+                "From " + a.getTripSource() + " to " + a.getTripDestination() + "\n" +
+                "Date of departure: " + a.getDepartureDate() + "\n" +
+                "Date of return: " + a.getReturnDate() + "\n" +
+                "Total passengers: " + a.getTotalPassengers() + "\n" +
+                "Total ticket price in Euros: " + a.getTotalTicketPrice());
+    }
         
         
     }
-}
+
